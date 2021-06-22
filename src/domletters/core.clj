11504dc0e4749351c1f5408809c4af96e629579a
Michael Bottini <mbottini1@gmail.com>
2021-06-22 14:37:08 -0700

    Add word parsing and reader input functions

--- src/domletters/core.clj
+++ src/domletters/core.clj
@@ -1,7 +1,23 @@
 (ns domletters.core
   (:gen-class))
 
+(defn is-alphabetical? [s]
+  (and 
+    (not (clojure.string/blank? s))
+    (every? #(Character/isLetter %) s)))
+
+(defn get-words-from-line [s]
+  (filter is-alphabetical? (clojure.string/split s #"\s+")))
+
+(defn get-words-from-reader [reader]
+  (let [line (.readLine reader)]
+    (if (nil? line)
+      '()
+      (concat (get-words-from-line line) (get-words-from-reader reader)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [reader (java.io.BufferedReader. *in*)]
+    (println (clojure.string/join " " (get-words-from-reader reader)))))
+
--- test/domletters/core_test.clj
+++ test/domletters/core_test.clj
@@ -5,3 +5,22 @@
 (deftest a-test
   (testing "I pass!"
     (is true)))
+
+(deftest test-is-alphabetical
+  (testing "These are alphabetical."
+    (is (every? is-alphabetical? ["abc" "sPoNgEbOb" "def"])))
+  (testing "These are not alphabetical."
+    (is (every? (complement is-alphabetical?) [",abc" "hjsas_eajs" "{}"])))
+  (testing "Blank strings are not alphabetical."
+    (is (not (is-alphabetical? "")))))
+
+(defn create-buffered-reader [s]
+  (->> s
+    java.io.StringReader.
+    java.io.BufferedReader.))
+
+(deftest test-get-words-from-reader
+  (testing "Getting strings from a constructed BufferedInputReader."
+    (is (= '("The" "quick" "brown" "fox")
+      (get-words-from-reader 
+        (create-buffered-reader "The ,quick\n\n quick br_wn\n brown fox. fox"))))))
