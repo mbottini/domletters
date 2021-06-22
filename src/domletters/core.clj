5e0b93145f4438d850a121b545c4e6c7f5645514
Michael Bottini <mbottini1@gmail.com>
2021-06-22 16:05:46 -0700

    Add docstrings to functions

--- src/domletters/core.clj
+++ src/domletters/core.clj
@@ -2,36 +2,43 @@
   (:gen-class))
 
 (defn is-alphabetical? [s]
+  "Determines whether a string contains only alphabetical characters."
   (and 
     (not (clojure.string/blank? s))
     (every? #(Character/isLetter %) s)))
 
 (defn get-words-from-line [s]
+  "Splits the string and returns only words containing only alphabetical 
+  characters."
   (filter is-alphabetical? (clojure.string/split s #"\s+")))
 
 (defn get-words-from-reader [reader]
+  "Gets all alphabetical words from a reader."
   (let [line (.readLine reader)]
     (if (nil? line)
       '()
       (concat (get-words-from-line line) (get-words-from-reader reader)))))
 
 (defn frequency-dict [word]
+  "Gets the frequency dictionary of the lower-case of a word."
   (frequencies (clojure.string/lower-case word)))
 
 (defn dominant-character-count [word]
+  "Gets the dominant character count of a word."
   (->> word
     frequency-dict
     vals
     (apply max)))
 
 (defn sum-counts [reader]
+  "Sums the dominant character count of every alphabetical word in the reader."
   (->> reader
     get-words-from-reader
     (map dominant-character-count)
     (apply +)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Main function. Uses stdin as the Reader to count dominant characters."
   [& args]
   (let [reader (java.io.BufferedReader. *in*)]
     (println (sum-counts reader))))
